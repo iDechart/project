50d6fef08efd89b96d22a297deea8d7a14ea8b01
iDechart <hallbjorn@yandex.ru>
2021-01-30 20:10:08 +0300

    uslojnenie funktsij

--- js/script4_practice2.js
+++ js/script4_practice2.js
@@ -1,6 +1,14 @@
 "use strict";
 let numberOfFilms
-numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start()
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,26 +18,57 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '')
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b
-    } else {
-        console.log('Error')
-        i--
+
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '')
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b
+        } else {
+            console.log('Error')
+            i--
+        }
     }
 }
 
-if (personalMovieDB.count < 10) {
-    console.log('Prosmotreno dovolno malo filmov')
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Vi klassicheskiy zritel')
-} else if ( personalMovieDB.count >= 30) {
-    console.log('Da vi konoman')
-} else {
-    console.log('Oshibka')
+rememberMyFilms()
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Prosmotreno dovolno malo filmov')
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Vi klassicheskiy zritel')
+    } else if ( personalMovieDB.count >= 30) {
+        console.log('Da vi konoman')
+    } else {
+        console.log('Oshibka')
+    }
+}
+detectPersonalLevel()
+
+// function showMyDB() {
+//     personalMovieDB.privat === false ? console.log(personalMovieDB) : console.log('Private information')
+// }
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB)
+    }
 }
 
-console.log(personalMovieDB);
+showMyDB(personalMovieDB.privat)
+
+function writeYourGenres() {
+    for (let i = 1; i < 4; i++) {
+        // const a = prompt(`Your favorite genre # ${i}`, '')
+        // personalMovieDB.genres.push(a)
+        personalMovieDB.genres[i - 1] = prompt(`Your favorite genre # ${i}`, '');
+
+    }
+}
+
+writeYourGenres()
+
 
