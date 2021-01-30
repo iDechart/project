5297ee010e5ab5e6a13e66be77ca8c74cbecb7fc
iDechart <hallbjorn@yandex.ru>
2021-01-30 20:09:02 +0300

    methods

---
+++ js/scriptMethods.js
@@ -0,0 +1,19 @@
+"use strict"
+// const str = "teSt";
+//
+// console.log(str.toLocaleLowerCase())
+
+const fruit = "Some fruit"
+
+console.log(fruit.indexOf("q"))
+
+const logg = "Hello world"
+
+console.log(logg.slice(6, (logg.length - 1)))
+
+const num = 12.2
+console.log(Math.round(num))
+
+const test = "12.2px"
+console.log(parseInt(test))
+console.log(parseFloat(num))
