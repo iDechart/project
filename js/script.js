ce1ebacb76e1d05253ff2e2a522579797d9b91a9
iDechart <hallbjorn@yandex.ru>
2021-01-30 20:09:24 +0300

    ; deletion

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict"
 let numberOfFilms
 numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
@@ -8,15 +8,14 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false
-};
+}
 
 const a = prompt('Один из последних просмотренных фильмов?', ''),
     b = prompt('На сколько оцените его?', ''),
     c = prompt('Один из последних просмотренных фильмов?', ''),
     d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+personalMovieDB.movies[a] = b
+personalMovieDB.movies[c] = d
 
-console.log(personalMovieDB);
-
+console.log(personalMovieDB)
